d1d86efbe380514cad765bc1777452bea9705d56
Cherkesov Sergey <go.for.broke1006@gmail.com>
2015-07-06 16:43:14 +0300

    Fix method which find part by color

--- Repo/PartRepo.php
+++ Repo/PartRepo.php
@@ -32,7 +32,7 @@ class PartRepo extends EntityRepository
      */
     public function findOneWithColorLike($color, $accuracy)
     {
-        $colorRepo = $this->_em->getRepository("ADWMosaicBundle:Color");
+        $colorRepo = $this->_em->getRepository("GFBMosaicBundle:Color");
 
         // Пробуем найти полное соответствие цвета
         $colors = $colorRepo->findBy(array(
@@ -41,34 +41,40 @@ class PartRepo extends EntityRepository
             "blue" => $color->getBlue(),
         ));
 
+        $criteria = new Criteria();
+        $expressionRed = $criteria->expr()->andX(
+            $criteria->expr()->gt("red", $color->getRed() - $accuracy),
+            $criteria->expr()->lt("red", $color->getRed() + $accuracy)
+        );
+        $expressionGreen = $criteria->expr()->andX(
+            $criteria->expr()->gt("green", $color->getGreen() - $accuracy),
+            $criteria->expr()->lt("green", $color->getGreen() + $accuracy)
+        );
+        $expressionBlue = $criteria->expr()->andX(
+            $criteria->expr()->gt("blue", $color->getBlue() - $accuracy),
+            $criteria->expr()->lt("blue", $color->getBlue() + $accuracy)
+        );
+
         // Если не получилось то ищем похожие цвета с некоторой погрешностью
         if (count($colors) == 0) {
-            $criteria = new Criteria();
-            $criteria->andWhere(
-                $criteria->expr()->orX(
-                    $criteria->expr()->gt("red", $color->getRed() - $accuracy),
-                    $criteria->expr()->lt("red", $color->getRed() + $accuracy)
-                )
-            );
-            $criteria->andWhere(
-                $criteria->expr()->orX(
-                    $criteria->expr()->gt("green", $color->getGreen() - $accuracy),
-                    $criteria->expr()->lt("green", $color->getGreen() + $accuracy)
-                )
-            );
-            $criteria->andWhere(
-                $criteria->expr()->orX(
-                    $criteria->expr()->gt("blue", $color->getBlue() - $accuracy),
-                    $criteria->expr()->lt("blue", $color->getBlue() + $accuracy)
-                )
-            );
-
+            $criteria->andWhere($expressionRed);
+            $criteria->andWhere($expressionGreen);
+            $criteria->andWhere($expressionBlue);
             $colors = $colorRepo->matching($criteria);
         }
 
+        // Если не получилось то ищем похожие цвета с огромной погрешностью!!!
+        /*if (count($colors) == 0) {
+            $criteria->orWhere($expressionRed);
+            $criteria->orWhere($expressionGreen);
+            $criteria->orWhere($expressionBlue);
+            $colors = $colorRepo->matching($criteria);
+        }*/
+
         if (count($colors) > 0) {
             // TODO: Нужно выбирать один из элементов рандомно
-            return $this->findOneBy(array("avgColor" => $color[0]));
+            $index = rand(0, count($colors) - 1);
+            return $this->findOneBy(array("avgColor" => $colors[$index]));
         }
 
         return null;
